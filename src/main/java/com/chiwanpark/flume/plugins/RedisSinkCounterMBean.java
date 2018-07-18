8505a3e9c29e618f1f6d30fa9471518dc37f8a8f
Philipp Hoffmann <ph@plista.com>
2018-07-18 16:50:52 +0200

    Register batch metrics

--- src/main/java/com/chiwanpark/flume/plugins/RedisSinkCounterMBean.java
+++ src/main/java/com/chiwanpark/flume/plugins/RedisSinkCounterMBean.java
@@ -4,4 +4,9 @@ public interface RedisSinkCounterMBean {
     public long getSinkSendTimeMicros();
     public long getSinkRollback();
     public long getSinkSuccess();
+
+    // note that we also need to put mbean accessor here which are available already via the base class
+    // apparently flume will only pick up the metrics for the mbean accessors found here
+    public long getBatchUnderflowCount();
+    public long getBatchCompleteCount();
 }
